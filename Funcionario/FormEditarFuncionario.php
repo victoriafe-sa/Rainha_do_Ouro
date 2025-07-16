abf74fb521d8626555878bc311efbd7fd894dc74
Natyelly Neres <natyelly.neres77@gmail.com>
2025-07-16 17:19:58 -0300

    funcionarios crud

--- Funcionario/FormEditarFuncionario.php
+++ Funcionario/FormEditarFuncionario.php
@@ -4,7 +4,7 @@
 
   <head>
     <meta charset="UTF-8">
-    <title>Editar Produto</title>
+    <title>Editar Funcionario</title>
     <link rel="stylesheet" href="css/estiloforms.css"/>
   </head>
 
@@ -12,33 +12,89 @@
     <?php
       include("../conectarbd.php");
       $recid=filter_input(INPUT_GET, 'editarid');
-      $selecionar= mysqli_query($conn, "SELECT * FROM tb_produtos WHERE id_produtos=$recid");
+      $selecionar= mysqli_query($conn, "SELECT * FROM tb_funcionarios WHERE id_funcionarios=$recid");
       $campo= mysqli_fetch_array($selecionar);
     ?>
 
     <div class="formulario">
-      <form method="post" action="EditarVenda.php">
+      <form method="post" action="EditarFuncionario.php">
      
-          <h1>Alterar Produto</h1>
+          <h1>Alterar Funcionario</h1>
      
 <!esta linha cria um campo oculto para passar o id_evento, pois senão ao clicar em Salvar o código não saberá onde salvar.-->
-        <input type="hidden" name="id" value="<?=$campo["id_produtos"]?>"> 
+<input type="hidden" name="id" value="<?=$campo["id"]?>"> 
 
-        <label>Nome</label><br> 
-        <input type="text" name="nome" placeholder="Nome" value="<?=$campo["nome"]?>"> <br><br>
+<label>Nome completo</label><br> 
+<input type="text" name="nome_completo" placeholder="Nome completo" value="<?=$campo["nome_completo"]?>"> <br><br>
 
-        <label>Tipo</label><br>
-        <input type="text" name="tipo" placeholder="tipo" value="<?=$campo["tipo"]?>"> <br><br>
-        
-        <label>valor de compra</label><br>
-        <input type="text" name="valor_compra" placeholder="valor de compra" value="<?=$campo["valor_compra"]?>"> <br><br>
-        
-        <label>valor de vendas</label><br>
-        <input type="text" name="valor_venda" placeholder="valor de venda" value="<?=$campo["valor_venda"]?>"> <br><br>
-        
-        <label>descrição:</label><br>
-        <input type="text" name="descricao" placeholder="descricao" value="<?=$campo["descricao"]?>"> <br><br>
+<label>Data de nascimento</label><br>
+<input type="date" name="data_nascimento" value="<?=$campo["data_nascimento"]?>"> <br><br>
 
+<label>CPF</label><br>
+<input type="text" name="cpf" placeholder="CPF" value="<?=$campo["cpf"]?>"> <br><br>
+
+<label>RG</label><br>
+<input type="text" name="rg" placeholder="RG" value="<?=$campo["rg"]?>"> <br><br>
+
+<label>Sexo</label><br>
+<input type="text" name="sexo" placeholder="Sexo" value="<?=$campo["sexo"]?>"> <br><br>
+
+<label>Estado civil</label><br>
+<input type="text" name="estado_civil" placeholder="Estado civil" value="<?=$campo["estado_civil"]?>"> <br><br>
+
+<label>Telefone</label><br>
+<input type="text" name="telefone" placeholder="Telefone" value="<?=$campo["telefone"]?>"> <br><br>
+
+<label>Email</label><br>
+<input type="email" name="email" placeholder="Email" value="<?=$campo["email"]?>"> <br><br>
+
+<label>CEP</label><br>
+<input type="text" name="cep" placeholder="CEP" value="<?=$campo["cep"]?>"> <br><br>
+
+<label>Rua</label><br>
+<input type="text" name="rua" placeholder="Rua" value="<?=$campo["rua"]?>"> <br><br>
+
+<label>Número</label><br>
+<input type="text" name="numero" placeholder="Número" value="<?=$campo["numero"]?>"> <br><br>
+
+<label>Bairro</label><br>
+<input type="text" name="bairro" placeholder="Bairro" value="<?=$campo["bairro"]?>"> <br><br>
+
+<label>Cidade</label><br>
+<input type="text" name="cidade" placeholder="Cidade" value="<?=$campo["cidade"]?>"> <br><br>
+
+<label>Estado</label><br>
+<input type="text" name="estado" placeholder="Estado" value="<?=$campo["estado"]?>"> <br><br>
+
+<label>Cargo</label><br>
+<input type="text" name="cargo" placeholder="Cargo" value="<?=$campo["cargo"]?>"> <br><br>
+
+<label>Data de admissão</label><br>
+<input type="date" name="data_admissao" value="<?=$campo["data_admissao"]?>"> <br><br>
+
+<label>Horário de trabalho</label><br>
+<input type="text" name="horario_trabalho" placeholder="Horário de trabalho" value="<?=$campo["horario_trabalho"]?>"> <br><br>
+
+<label>Salário</label><br>
+<input type="text" name="salario" placeholder="Salário" value="<?=$campo["salario"]?>"> <br><br>
+
+<label>Tipo de contrato</label><br>
+<input type="text" name="tipo_contrato" placeholder="Tipo de contrato" value="<?=$campo["tipo_contrato"]?>"> <br><br>
+
+<label>Carteira de trabalho</label><br>
+<input type="text" name="carteira_trabalho" placeholder="Carteira de trabalho" value="<?=$campo["carteira_trabalho"]?>"> <br><br>
+
+<label>PIS</label><br>
+<input type="text" name="pis" placeholder="PIS" value="<?=$campo["pis"]?>"> <br><br>
+
+<label>Status</label><br>
+<input type="text" name="status" placeholder="Status" value="<?=$campo["status"]?>"> <br><br>
+
+<label>Observações</label><br>
+<input type="text" name="observacoes" placeholder="Observações" value="<?=$campo["observacoes"]?>"> <br><br>
+
+<label>Data de cadastro</label><br>
+<input type="date" name="data_cadastro" value="<?=$campo["data_cadastro"]?>"> <br><br>
 
         <input type="submit" class="botoes" value="Salvar" >
         <a href="FormConsultarProduto.php"><input type="button" class="botoes" value="Cancelar"/></a>
