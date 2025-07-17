cceecb3a01493d922b4442fce7ad4f39ddadecd0
victoriafe-sa <victoria.senac.13@gmail.com>
2025-07-17 17:39:56 -0300

    removendo impedimetos

--- index.php
+++ index.php
@@ -10,24 +10,13 @@
         <nav class="menu">
             <ul>
                 <li><a href="index.html">Home</a></li>
-                <li><a href="#">Veiculo</a>
+                <li><a href="#">Funcionario</a>
                     <ul>
-                        <li><a href="Veiculo/FormCadastrarVeiculo.html">Cadastrar</a></li>
-                        <li><a href="Veiculo/FormConsultarVeiculo.php">Consultar</a>
+                        <li><a href="Funcionario/FormCadastrarFuncionario.html">Cadastrar</a></li>
+                        <li><a href="Funcionario/FormConsultarFuncionario.php">Consultar</a>
                     </ul>
                 </li>
-                <li><a href="#">Venda</a>
-                    <ul>
-                        <li><a href="Venda/FormCadastrarVenda.html">Cadastrar</a></li>
-                        <li><a href="Venda/FormConsultarVenda.php">Consultar</a>
-                    </ul>
-                </li>
-                <li><a href="#">Vendedor</a>
-                    <ul>
-                        <li><a href="Vendedor/FormCadastrarVendedor.html">Cadastrar</a></li>
-                        <li><a href="Vendedor/FormConsultarVendedor.php">Consultar</a>
-                    </ul>
-                </li>
+                
             </ul>
             
         </nav>
