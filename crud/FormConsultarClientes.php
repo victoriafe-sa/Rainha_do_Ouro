04daf5a5e2bfbafbd0ddfeaae7b3230321bd4410
Victoria Ferreira <victoria.senac.13@gmail.com>
2025-08-08 12:23:41 -0300

    filtro de pesquisa

--- crud/FormConsultarClientes.php
+++ crud/FormConsultarClientes.php
@@ -1,60 +1,98 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <title>Consultar Clientes</title>
-        <link type="text/css" rel="stylesheet" href="../css/consultar.css">
-        <link rel="shortcut icon" type="imagex/png" href="../img/RAINHA DO OURO.ico">
-    </head>
 
-     <body>
-                <h1>Clientes Cadastrados</h1>
-                <table
-                   width="100%"
-                   border="1" 
-                   bordercolor="black"
-                   cellspacing="2" 	
-                   cellpadding="5"
-                   >
-                    <tr>
-                        <td align="center"> <strong>ID</strong></td>	
-                        <td align="center"> <strong>Nome</strong></td>
-                        <td align="center"> <strong>Telefone</strong></td>
-                        <td align="center"> <strong>CPF</strong></td>
-                        <td align="center"> <strong>Data de Nascimento</strong></td>
-                        <td align="center"> <strong>Email</strong></td>
-                        <td align="center"> <strong>Senha</strong></td>
-                        <td align="center"> <strong>Bairro</strong></td>
-                        <td align="center"> <strong>Cidade</strong></td>
-                        <td align="center"> <strong>Estado</strong></td>
-                        <td align="center"> <strong>Ativo</strong></td>
-                        <td align="center"> <strong>Editar</strong></td>
-                        <td align="center"> <strong>Excluir</strong></td>
+<head>
+    <meta charset="UTF-8">
+    <title>Consultar Clientes</title>
+    <link type="text/css" rel="stylesheet" href="../css/consultar.css">
+    <link rel="shortcut icon" type="imagex/png" href="../img/RAINHA DO OURO.ico">
+</head>
+
+<body>
+    <h1>Clientes Cadastrados</h1>
+<!--Seção de pesquisa-->
+    <form method="GET" action="">
+        <label for="nome">Nome:</label>
+        <input type="text" name="nome" id="nome" />
+
+        <label for="cpf">CPF:</label>
+        <input type="text" name="cpf" id="cpf" />
+
+        <label for="ativo">Ativo:</label>
+        <select name="ativo" id="ativo">
+            <option value="">Todos</option>
+            <option value="1">Sim</option>
+            <option value="0">Não</option>
+        </select>
+
+        <input type="submit" value="Pesquisar" class="botoes" />
+    </form>
+    <br>
+
+    <table width="100%" border="1" bordercolor="black" cellspacing="2" cellpadding="5">
+        <tr>
+            <td align="center"> <strong>ID</strong></td>
+            <td align="center"> <strong>Nome</strong></td>
+            <td align="center"> <strong>Telefone</strong></td>
+            <td align="center"> <strong>CPF</strong></td>
+            <td align="center"> <strong>Data de Nascimento</strong></td>
+            <td align="center"> <strong>Email</strong></td>
+            <td align="center"> <strong>Senha</strong></td>
+            <td align="center"> <strong>Bairro</strong></td>
+            <td align="center"> <strong>Cidade</strong></td>
+            <td align="center"> <strong>Estado</strong></td>
+            <td align="center"> <strong>Ativo</strong></td>
+            <td align="center"> <strong>Editar</strong></td>
+            <td align="center"> <strong>Excluir</strong></td>
 
 
-                    </tr>
+        </tr>
 
-                    <?php
-                        include("../conectarbd.php");
-                        $selecionar= mysqli_query($conn, "SELECT * FROM tb_clientes");
-                        while ($campo= mysqli_fetch_array($selecionar)){?>
-                            <tr>
-                                <td align="center"><?=$campo["id_clientes"]?></td>
-                                <td align="center"><?=$campo["nome"]?></td>
-                                <td align="center"><?=$campo["telefone"]?></td>
-                                <td align="center"><?=$campo["cpf"]?></td>
-                                <td align="center"><?=$campo["data_nascimento"]?></td>
-                                <td align="center"><?=$campo["email"]?></td>
-                                <td align="center"><?=$campo["senha"]?></td>
-                                <td align="center"><?=$campo["bairro"]?></td>
-                                <td align="center"><?=$campo["cidade"]?></td>
-                                <td align="center"><?=$campo["estado"]?></td>
-                                <td align="center"><?=$campo["ativo"]?></td>
-                                <td align="center"><a href="../crud/FormEditarClientes.php?editarid=<?php echo $campo ['id_clientes'];?>">Editar</a></td>
-                                <td align="center"><a href="../crud/ExcluirClientes.php?p=excluir&clientes=<?php echo $campo['id_clientes'];?>">Excluir</a></td>
-                            </tr>
-                    <?php }?>
-                </table><br>
-                    <a href="../html/dashboard_adm.php"><input type="button" class="botoes" value="Cancelar"/></a>
-    </body>
+        <?php
+        include("../conectarbd.php");
+        //$selecionar = mysqli_query($conn, "SELECT * FROM tb_clientes");
+
+        $nome = isset($_GET['nome']) ? $_GET['nome'] : '';
+        $cpf = isset($_GET['cpf']) ? $_GET['cpf'] : '';
+        $ativo = isset($_GET['ativo']) ? $_GET['ativo'] : '';
+
+        // Monta a consulta SQL com filtros opcionais
+        $sql = "SELECT * FROM tb_clientes WHERE 1=1";
+
+        if (!empty($nome)) {
+            $sql .= " AND nome LIKE '%" . mysqli_real_escape_string($conn, $nome) . "%'";
+        }
+        if (!empty($cpf)) {
+            $sql .= " AND cpf LIKE '%" . mysqli_real_escape_string($conn, $cpf) . "%'";
+        }
+        if ($ativo !== '') {
+            $sql .= " AND ativo = '" . mysqli_real_escape_string($conn, $ativo) . "'";
+        }
+
+        $selecionar = mysqli_query($conn, $sql);
+
+        while ($campo = mysqli_fetch_array($selecionar)) { ?>
+            <tr>
+                <td align="center"><?= $campo["id_clientes"] ?></td>
+                <td align="center"><?= $campo["nome"] ?></td>
+                <td align="center"><?= $campo["telefone"] ?></td>
+                <td align="center"><?= $campo["cpf"] ?></td>
+                <td align="center"><?= $campo["data_nascimento"] ?></td>
+                <td align="center"><?= $campo["email"] ?></td>
+                <td align="center"><?= $campo["senha"] ?></td>
+                <td align="center"><?= $campo["bairro"] ?></td>
+                <td align="center"><?= $campo["cidade"] ?></td>
+                <td align="center"><?= $campo["estado"] ?></td>
+                <td align="center"><?= $campo["ativo"] ?></td>
+                <td align="center"><a
+                        href="../crud/FormEditarClientes.php?editarid=<?php echo $campo['id_clientes']; ?>">Editar</a></td>
+                <td align="center"><a
+                        href="../crud/ExcluirClientes.php?p=excluir&clientes=<?php echo $campo['id_clientes']; ?>">Excluir</a>
+                </td>
+            </tr>
+        <?php } ?>
+    </table><br>
+    <a href="../html/dashboard_adm.php"><input type="button" class="botoes" value="Cancelar" /></a>
+</body>
+
 </html>
--- crud/FormConsultarFuncionario.php
+++ crud/FormConsultarFuncionario.php
@@ -1,62 +1,100 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <title>Consultar Funcionário</title>
-        <link type="text/css" rel="stylesheet" href="../css/consultar.css">
-        <link rel="shortcut icon" type="imagex/png" href="../img/RAINHA DO OURO.ico">
-    </head>
 
-     <body>
-                <h1>Funcionários Cadastrados</h1>
-                <table
-                   width="100%"
-                   border="1" 
-                   bordercolor="black"
-                   cellspacing="2" 	
-                   cellpadding="5"
-                   >
-                    <tr>
-                    <td align="center"> <strong>ID</strong></td>	
-                    <td align="center"> <strong>Nome</strong></td>	
-                    <td align="center"> <strong>Data de Nascimento</strong></td>	
-                    <td align="center"> <strong>CPF</strong></td>	
-                    <td align="center"> <strong>Telefone</strong></td>	
-                    <td align="center"> <strong>E-mail</strong></td>	
-                    <td align="center"> <strong>Bairro</strong></td>	
-                    <td align="center"> <strong>Cidade</strong></td>	
-                    <td align="center"> <strong>Cargo</strong></td>	
-                    <td align="center"> <strong>Data de admissão</strong></td>	
-                    <td align="center"> <strong>Horário de trabalho</strong></td>	
-                    <td align="center"> <strong>Tipo de contrato</strong></td>	
-                    <td align="center"> <strong>Status</strong></td>
-                        <td width="10"> <strong>Editar</strong></td>
-                        <td width="10"> <strong>Deletar</strong></td>
-                    </tr>
+<head>
+    <meta charset="UTF-8">
+    <title>Consultar Funcionário</title>
+    <link type="text/css" rel="stylesheet" href="../css/consultar.css">
+    <link rel="shortcut icon" type="imagex/png" href="../img/RAINHA DO OURO.ico">
+</head>
 
-                    <?php
-                        include("../conectarbd.php");
-                        $selecionar= mysqli_query($conn, "SELECT * FROM tb_funcionarios");
-                        while ($campo= mysqli_fetch_array($selecionar)){?>
-                            <tr>
-                            <td align="center"><?=$campo["id_funcionarios"]?></td>
-                            <td align="center"><?=$campo["nome_completo"]?></td>
-                            <td align="center"><?=$campo["data_nascimento"]?></td>
-                            <td align="center"><?=$campo["cpf"]?></td>
-                            <td align="center"><?=$campo["telefone"]?></td>
-                            <td align="center"><?=$campo["email"]?></td>
-                            <td align="center"><?=$campo["bairro"]?></td>
-                            <td align="center"><?=$campo["cidade"]?></td>
-                            <td align="center"><?=$campo["cargo"]?></td>
-                            <td align="center"><?=$campo["data_cadastro"]?></td>
-                            <td align="center"><?=$campo["horario_trabalho"]?></td>
-                            <td align="center"><?=$campo["tipo_contrato"]?></td>
-                            <td align="center"><?=$campo["status"]?></td>
-                                <td align="center"><a href="../crud/FormEditarFuncionario.php?editarid=<?php echo $campo ['id_funcionarios'];?>">Editar</a></td>
-                                <td align="center"><i><a href="../crud/ExcluirFuncionario.php?p=excluir&funcionario=<?php echo $campo['id_funcionarios'];?>">Excluir</i></a></td>
-                            </tr>
-                    <?php }?>
-                </table><br>
-                    <a href="../html/dashboard_adm.php"><input type="button" class="botoes" value="Cancelar"/></a>
-    </body>
+<body>
+    <h1>Funcionários Cadastrados</h1>
+
+    <form method="GET" action="">
+        <label for="nome">Nome:</label>
+        <input type="text" name="nome" id="nome" value="<?php echo isset($_GET['nome']) ? $_GET['nome'] : ''; ?>">
+
+        <label for="cpf">CPF:</label>
+        <input type="text" name="cpf" id="cpf" value="<?php echo isset($_GET['cpf']) ? $_GET['cpf'] : ''; ?>">
+
+        <label for="cargo">Cargo:</label>
+        <input type="text" name="cargo" id="cargo" value="<?php echo isset($_GET['cargo']) ? $_GET['cargo'] : ''; ?>">
+
+        <input type="submit" value="Pesquisar" class="botoes">
+    </form>
+    <br>
+
+
+    <table width="100%" border="1" bordercolor="black" cellspacing="2" cellpadding="5">
+        <tr>
+            <td align="center"> <strong>ID</strong></td>
+            <td align="center"> <strong>Nome</strong></td>
+            <td align="center"> <strong>Data de Nascimento</strong></td>
+            <td align="center"> <strong>CPF</strong></td>
+            <td align="center"> <strong>Telefone</strong></td>
+            <td align="center"> <strong>E-mail</strong></td>
+            <td align="center"> <strong>Bairro</strong></td>
+            <td align="center"> <strong>Cidade</strong></td>
+            <td align="center"> <strong>Cargo</strong></td>
+            <td align="center"> <strong>Data de admissão</strong></td>
+            <td align="center"> <strong>Horário de trabalho</strong></td>
+            <td align="center"> <strong>Tipo de contrato</strong></td>
+            <td align="center"> <strong>Status</strong></td>
+            <td width="10"> <strong>Editar</strong></td>
+            <td width="10"> <strong>Deletar</strong></td>
+        </tr>
+
+        <?php
+        include("../conectarbd.php");
+        
+        //$selecionar = mysqli_query($conn, "SELECT * FROM tb_funcionarios");
+
+        $nome = isset($_GET['nome']) ? $_GET['nome'] : '';
+        $cpf = isset($_GET['cpf']) ? $_GET['cpf'] : '';
+        $cargo = isset($_GET['cargo']) ? $_GET['cargo'] : '';
+
+        // Consulta com filtros dinâmicos
+        $sql = "SELECT * FROM tb_funcionarios WHERE 1=1";
+
+        if (!empty($nome)) {
+            $sql .= " AND nome_completo LIKE '%" . mysqli_real_escape_string($conn, $nome) . "%'";
+        }
+        if (!empty($cpf)) {
+            $sql .= " AND cpf LIKE '%" . mysqli_real_escape_string($conn, $cpf) . "%'";
+        }
+        if (!empty($cargo)) {
+            $sql .= " AND cargo LIKE '%" . mysqli_real_escape_string($conn, $cargo) . "%'";
+        }
+
+        $selecionar = mysqli_query($conn, $sql);
+
+
+        while ($campo = mysqli_fetch_array($selecionar)) { ?>
+            <tr>
+                <td align="center"><?= $campo["id_funcionarios"] ?></td>
+                <td align="center"><?= $campo["nome_completo"] ?></td>
+                <td align="center"><?= $campo["data_nascimento"] ?></td>
+                <td align="center"><?= $campo["cpf"] ?></td>
+                <td align="center"><?= $campo["telefone"] ?></td>
+                <td align="center"><?= $campo["email"] ?></td>
+                <td align="center"><?= $campo["bairro"] ?></td>
+                <td align="center"><?= $campo["cidade"] ?></td>
+                <td align="center"><?= $campo["cargo"] ?></td>
+                <td align="center"><?= $campo["data_cadastro"] ?></td>
+                <td align="center"><?= $campo["horario_trabalho"] ?></td>
+                <td align="center"><?= $campo["tipo_contrato"] ?></td>
+                <td align="center"><?= $campo["status"] ?></td>
+                <td align="center"><a
+                        href="../crud/FormEditarFuncionario.php?editarid=<?php echo $campo['id_funcionarios']; ?>">Editar</a>
+                </td>
+                <td align="center"><i><a
+                            href="../crud/ExcluirFuncionario.php?p=excluir&funcionario=<?php echo $campo['id_funcionarios']; ?>">Excluir</i></a>
+                </td>
+            </tr>
+        <?php } ?>
+    </table><br>
+    <a href="../html/dashboard_adm.php"><input type="button" class="botoes" value="Cancelar" /></a>
+</body>
+
 </html>
